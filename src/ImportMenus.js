d0e149354db4fdd0d6178a75da535c23b2f6bdb2
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-01-27 10:08:43 +1300

    Added tags, menupageUrl and imageUrl to menu importer

--- src/ImportMenus.js
+++ src/ImportMenus.js
@@ -38,18 +38,36 @@ const start = async () => {
         }
 
         const splittedRows = ImmutableEx.splitIntoChunks(Immutable.fromJS(data).skip(1), 10); // Skipping the first item as it is the CSV header
-        const columns = OrderedSet.of('username', 'en_NZ_name', 'zh_name', 'jp_name', 'en_NZ_description', 'zh_description', 'jp_description');
+        const columns = OrderedSet.of(
+          'username',
+          'en_NZ_name',
+          'zh_name',
+          'jp_name',
+          'en_NZ_description',
+          'zh_description',
+          'jp_description',
+          'menuPageUrl',
+          'imageUrl',
+          'tags',
+        );
 
         await BluebirdPromise.each(splittedRows.toArray(), rowChunck =>
           Promise.all(rowChunck.map(async (rawRow) => {
             const values = Common.extractColumnsValuesFromRow(columns, Immutable.fromJS(rawRow));
             const user = await Common.getUser(values.get('username'));
             const menus = await Common.loadAllMenus(user, { name: values.get('en_NZ_name') });
+            const tags = await Common.loadAllTags(user);
+            const tagsToFind = Immutable.fromJS(values.get('tags').split('|'))
+              .map(_ => _.trim())
+              .filterNot(_ => _.length === 0);
             const info = Map({
               ownedByUser: user,
               maintainedByUsers: List.of(user),
               name: Map({ en_NZ: values.get('en_NZ_name'), zh: values.get('zh_name'), jp: values.get('jp_name') }),
               description: Map({ en_NZ: values.get('en_NZ_description'), zh: values.get('zh_description'), jp: values.get('jp_description') }),
+              menuPageUrl: values.get('menuPageUrl'),
+              imageUrl: values.get('imageUrl'),
+              tagIds: tags.filter(tag => tagsToFind.find(_ => _.localeCompare(tag.getIn(['name', 'en_NZ'])) === 0)).map(tag => tag.get('id')),
             });
 
             if (menus.isEmpty()) {
