0ef0f1f2d2a3362f62fbbf2c51164bb3cf673e5d
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-02-16 19:26:41 +1300

    Added feature in table importer to support importing table indecies

--- src/ImportTables.js
+++ src/ImportTables.js
@@ -38,35 +38,39 @@ const start = async () => {
         }
 
         const splittedRows = ImmutableEx.splitIntoChunks(Immutable.fromJS(data).skip(1), 10); // Skipping the first item as it is the CSV header
-        const columns = OrderedSet.of('username', 'restaurantName', 'en_NZ_name', 'zh_name', 'jp_name', 'tableState');
+        const columns = OrderedSet.of('username', 'restaurantName', 'en_NZ_name', 'zh_name', 'jp_name', 'tableState', 'sortOrderIndex');
 
         await BluebirdPromise.each(splittedRows.toArray(), rowChunck =>
-          Promise.all(rowChunck.map(async (rawRow) => {
-            const values = Common.extractColumnsValuesFromRow(columns, Immutable.fromJS(rawRow));
-            const user = await Common.getUser(values.get('username'));
-            const restaurantId = (await Common.loadAllRestaurants(user, { name: values.get('restaurantName') })).first().get('id');
-            const tables = await Common.loadAllTables(user, restaurantId, { name: values.get('en_NZ_name') });
-            const info = Map({
-              ownedByUser: user,
-              maintainedByUsers: List.of(user),
-              restaurantId,
-              name: Map({ en_NZ: values.get('en_NZ_name'), zh: values.get('zh_name'), jp: values.get('jp_name') }),
-              tableStateId: tableStates.find(tableState => tableState.get('key').localeCompare(values.get('tableState')) === 0).get('id'),
-            });
+          Promise.all(
+            rowChunck.map(async rawRow => {
+              const values = Common.extractColumnsValuesFromRow(columns, Immutable.fromJS(rawRow));
+              const user = await Common.getUser(values.get('username'));
+              const restaurantId = (await Common.loadAllRestaurants(user, { name: values.get('restaurantName') })).first().get('id');
+              const tables = await Common.loadAllTables(user, restaurantId, { name: values.get('en_NZ_name') });
+              const info = Map({
+                ownedByUser: user,
+                maintainedByUsers: List.of(user),
+                restaurantId,
+                name: Map({ en_NZ: values.get('en_NZ_name'), zh: values.get('zh_name'), jp: values.get('jp_name') }),
+                tableStateId: tableStates.find(tableState => tableState.get('key').localeCompare(values.get('tableState')) === 0).get('id'),
+                sortOrderIndex: parseInt(values.get('sortOrderIndex'), 10),
+              });
 
-            if (tables.isEmpty()) {
-              const acl = ParseWrapperService.createACL(user);
+              if (tables.isEmpty()) {
+                const acl = ParseWrapperService.createACL(user);
 
-              acl.setPublicReadAccess(true);
-              acl.setRoleWriteAccess('administrators', true);
+                acl.setPublicReadAccess(true);
+                acl.setRoleWriteAccess('administrators', true);
 
-              await tableService.create(info, acl, null, true);
-            } else if (tables.count() === 1) {
-              await tableService.update(tables.first().merge(info), null, true);
-            } else {
-              console.error(`Multiple tables found with username ${values.get('username')} and table name: ${values.get('en_NZ_name')}`);
-            }
-          })));
+                await tableService.create(info, acl, null, true);
+              } else if (tables.count() === 1) {
+                await tableService.update(tables.first().merge(info), null, true);
+              } else {
+                console.error(`Multiple tables found with username ${values.get('username')} and table name: ${values.get('en_NZ_name')}`);
+              }
+            }),
+          ),
+        );
       },
     );
 
