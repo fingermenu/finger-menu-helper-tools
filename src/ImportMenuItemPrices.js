6ee0eea01b09ac7953a084e0a5bfba49774b6502
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-04-21 12:25:21 +1200

    Added must choose size, must choose dietary option, default choice item prices for menu item price and min and max of side dishes

--- src/ImportMenuItemPrices.js
+++ src/ImportMenuItemPrices.js
@@ -37,7 +37,18 @@ const start = async () => {
         }
 
         const splittedRows = ImmutableEx.splitIntoChunks(Immutable.fromJS(data).skip(1), 10); // Skipping the first item as it is the CSV header
-        const columns = OrderedSet.of('username', 'menuItemName', 'choiceItemDescriptions', 'currentPrice', 'tags');
+        const columns = OrderedSet.of(
+          'username',
+          'menuItemName',
+          'choiceItemDescriptions',
+          'currentPrice',
+          'tags',
+          'defaultChoiceItemDescriptions',
+          'mustChooseSize',
+          'mustChooseDietaryOption',
+          'minNumberOfSideDishes',
+          'maxNumberOfSideDishes',
+        );
 
         await BluebirdPromise.each(splittedRows.toArray(), rowChunck =>
           Promise.all(
@@ -51,17 +62,28 @@ const start = async () => {
               const menuItemId = (await Common.loadAllMenuItems(user, { name: values.get('menuItemName') })).first().get('id');
               const menuItemPrices = await Common.loadAllMenuItemPrices(user, { menuItemId });
               const choiceItems = await Common.loadAllChoiceItems(user);
+              const allChoiceItemPrices = await Common.loadAllChoiceItemPrices(user);
+
               const choiceItemsToFind = Immutable.fromJS(values.get('choiceItemDescriptions').split('|'))
                 .map(_ => _.trim())
                 .filterNot(_ => _.length === 0);
               const choiceItemIdsToFind = choiceItems
                 .filter(choiceItem => choiceItemsToFind.find(_ => _.localeCompare(choiceItem.getIn(['description', 'en_NZ'])) === 0))
                 .map(choiceItem => choiceItem.get('id'));
-              const choiceItemPrices = (await Common.loadAllChoiceItemPrices(user)).map(_ =>
-                _.set('choiceItem', new ChoiceItem(_.get('choiceItem')).getInfo()),
-              );
+              const choiceItemPrices = choiceItemPrices.map(_ => _.set('choiceItem', new ChoiceItem(_.get('choiceItem')).getInfo()));
               const choiceItemPriceIds = choiceItemPrices
                 .filter(choiceItemPrice => choiceItemIdsToFind.find(_ => _.localeCompare(choiceItemPrice.get('choiceItemId')) === 0))
+                .map(_ => _.get('id'));
+
+              const defaultChoiceItemsToFind = Immutable.fromJS(values.get('defaultChoiceItemDescriptions').split('|'))
+                .map(_ => _.trim())
+                .filterNot(_ => _.length === 0);
+              const defaultChoiceItemIdsToFind = choiceItems
+                .filter(choiceItem => defaultChoiceItemsToFind.find(_ => _.localeCompare(choiceItem.getIn(['description', 'en_NZ'])) === 0))
+                .map(choiceItem => choiceItem.get('id'));
+              const defaultChoiceItemPrices = allChoiceItemPrices.map(_ => _.set('choiceItem', new ChoiceItem(_.get('choiceItem')).getInfo()));
+              const defaultChoiceItemPriceIds = defaultChoiceItemPrices
+                .filter(choiceItemPrice => defaultChoiceItemIdsToFind.find(_ => _.localeCompare(choiceItemPrice.get('choiceItemId')) === 0))
                 .map(_ => _.get('id'));
 
               const choiceItemPriceSortOrderIndices = choiceItemPriceIds
@@ -85,7 +107,14 @@ const start = async () => {
                 addedByUser: user,
                 menuItemId,
                 choiceItemPriceIds,
+                defaultChoiceItemPriceIds,
                 currentPrice: parseFloat(values.get('currentPrice')),
+                rules: Map({
+                  mustChooseSize: 'T'.localeCompare(values.get('mustChooseSize')) === 0,
+                  mustChooseDietaryOption: 'T'.localeCompare(values.get('mustChooseDietaryOption')) === 0,
+                  minNumberOfSideDishes: parseInt(values.get('minNumberOfSideDishes'), 10),
+                  maxNumberOfSideDishes: parseInt(values.get('maxNumberOfSideDishes'), 10),
+                }),
                 choiceItemPriceSortOrderIndices,
                 tagIds: tags.filter(tag => tagsToFind.find(_ => _.localeCompare(tag.getIn(['name', 'en_NZ'])) === 0)).map(tag => tag.get('id')),
               });
